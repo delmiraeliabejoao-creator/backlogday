edece4aa9537e7a62862a0592230a0bfa5abf01b
delmiraeliabejoao-creator <delmiraeliabejoao@gmail.com>
2026-08-02 16:43:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 from datetime import datetime
 from fpdf import FPDF
 from dados import *
-from banco import *  
-
+from banco import *
 # 🎨 CONFIGURAÇÃO INICIAL
 st.set_page_config(
     page_title="BACKLOGDAY",
